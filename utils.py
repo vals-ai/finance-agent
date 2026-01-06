10d397a3571d30aaa74fb20f919e94636e52698b
Langston Nashold <lgnashold@gmail.com>
2026-01-06 00:00:19 -0800

    Move prompt from utils to its own file; add create_override_config

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from model_library.base import LLMConfig
 
 TOKEN_KEYS = [
     "in_tokens",
@@ -21,24 +22,6 @@ COST_KEYS = [
     "total",
 ]
 
-
-INSTRUCTIONS_PROMPT = """You are a financial agent. Today is April 07, 2025. You are given a question and you need to answer it using the tools provided.
-You may not interract with the user.
-When you have the answer, you should respond with 'FINAL ANSWER:' followed by your answer.
-At the end of your answer, you should provide your sources in a dictionary with the following format:
-{{
-    "sources": [
-        {{
-            "url": "https://example.com",
-            "name": "Name of the source"
-        }},
-        ...
-    ]
-}}
-
-Question:
-{question}
-"""
 
 
 def _merge_statistics(metadata: dict) -> dict:
@@ -84,3 +67,13 @@ def _merge_statistics(metadata: dict) -> dict:
         metadata["total_duration_seconds"] = (end - start).total_seconds()
 
     return metadata
+
+def create_override_config(**kwargs) -> LLMConfig:
+    # Filter kwargs to only include valid LLMConfig fields
+    valid_kwargs = {k: v for k, v in kwargs.items() if k in LLMConfig.model_fields}
+
+    # hardcode fix for max output tokens
+    if "max_output_tokens" in kwargs and "max_tokens" not in kwargs:
+        valid_kwargs["max_tokens"] = kwargs["max_output_tokens"]
+
+    return LLMConfig(**valid_kwargs)
