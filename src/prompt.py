ff9931268164d05760e09c89ac695f372ae23dad
OrestesK <oresteskassian@gmail.com>
2026-01-28 12:58:04 -0800

    change: submit_final_result Tool

--- src/prompt.py
+++ src/prompt.py
@@ -1,6 +1,6 @@
 INSTRUCTIONS_PROMPT = """You are a financial agent. Today is April 07, 2025. You are given a question and you need to answer it using the tools provided.
-You may not interract with the user.
-When you have the answer, you should respond with 'FINAL ANSWER:' followed by your answer.
+You may not interact with the user.
+When you have the final answer, you should call the `submit_final_result` tool with it.
 At the end of your answer, you should provide your sources in a dictionary with the following format:
 {{
     "sources": [
